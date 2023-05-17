17a42ec2435a5a93bcc061e7d95b3332f740232c
jasbel <asbel.elmec@gmail.com>
2023-05-17 18:37:18 -0400

    feat: changes

---
+++ src/app/signal/signal.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignalComponent } from './signal.component';
+
+
+
+@NgModule({
+  declarations: [
+    SignalComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SignalModule { }
